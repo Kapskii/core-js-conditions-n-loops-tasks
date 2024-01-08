548e255e954d4920daccaa4069e57bf913da2d49
Kapskii <kapskij20@gmail.com>
2024-01-08 11:22:54 +0300

    add tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -353,8 +353,20 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const sortArr = arr;
+
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    for (let j = 0; j < arr.length - i - 1; j += 1) {
+      if (sortArr[j] > sortArr[j + 1]) {
+        const temp = sortArr[j];
+        sortArr[j] = sortArr[j + 1];
+        sortArr[j + 1] = temp;
+      }
+    }
+  }
+
+  return sortArr;
 }
 
 /**
